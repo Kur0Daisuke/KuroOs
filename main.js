b201cdd125b2f2aabcc04650bb9f870f5acc6d02
Kuro <kyawphyozan78@gmail.com>
2024-01-12 22:10:15 +0630

    Changes and tests

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// const terminalDiv = document.querySelector(".terminal");
-// const terminal = new Terminal(terminalDiv, ActionLibrary);
+const terminalDiv = document.querySelector(".terminal");
+const terminal = new Terminal(terminalDiv, ActionLibrary);
 // const Os = new OS();
 // Os.Load();
-const terminal = new Terminal
 // colorMode | 0 - default | 1 - pastel mode | 2 - Custom Color
